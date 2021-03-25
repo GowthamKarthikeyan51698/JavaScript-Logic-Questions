d06aca2f387d8d884fe3a718b8c4e7f1d60e7dad
GowthamKarthikeyan51698 <75258383+GowthamKarthikeyan51698@users.noreply.github.com>
2021-03-25 10:25:47 +0530

    Add files via upload

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+function fibonacci_num(num, n1, n2){
+    let i, n3;
+    console.log(n1);
+    console.log(n2);
+    for(i=2;i<num;i++){
+        n3 = n1+n2;
+        console.log(n3);
+        n1 = n2;
+        n2 = n3;
+    }
+}
+
+
+let number = 15,
+    n1 = 0,
+    n2 = 1;
+fibonacci_num(number, n1, n2)
